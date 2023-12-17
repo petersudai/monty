0a83716d4bb8e19bd08010117dfb94174dbebdbf
petersudai <psudai@gmail.com>
2023-12-17 11:34:02 -0800

    add

--- pall.c
+++ pall.c
@@ -13,6 +13,12 @@ void pall(stack_t **stack, unsigned int line_number)
 
 	current = *stack;
 
+	if (!current)
+	{
+		fprintf(stderr, "L%d: can't pall, stack is empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
